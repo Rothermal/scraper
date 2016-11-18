3de442e534efa89d07fb049a97b73c7bbfd71149
Mark Rothermal <rothermal2@comcast.net>
2016-11-17 22:59:55 -0600

    initial commit

---
+++ server.js
@@ -0,0 +1,17 @@
+var request = require('request');
+var cheerio = require('cheerio');
+var areaCode = 55316;
+var url = "http://www.wunderground.com/cgi-bin/findweather/getForecast?&query=" + 55316;
+
+
+request(url,function(error, response, body){
+    if(!error) {
+        var $ = cheerio.load(response.body),
+            temperture = $("[data-variable = 'temperture'].wx-value").html();
+        console.log("Its " + temperture + " degrees Farenheit");
+        //console.log(body);
+        //console.log(response);
+    }else {
+        console.log("We had an error ", error);
+    }
+});
